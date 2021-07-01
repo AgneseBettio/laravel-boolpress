f1d109b86c5c5658da3cb92015106ddbb5b13d87
AgneseBettio <bettio.agnese@gmail.com>
2021-07-01 21:27:46 +0200

    impostate CRUD

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,16 @@
 @extends('layouts.public')
+@section('content')
+
+    @foreach($posts as $post) 
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{$post->title}}</h5>
+            <p class="card-text">{{$post->text}}</p>
+        </div>
+    </div>
+
+    {{-- per eventuale form per commentare?? --}}
+    <a href="#">commenta</a>
+    @endforeach
+@endsection
+
