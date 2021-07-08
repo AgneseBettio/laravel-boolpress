9c8afa51b1d76d50b6063e278589d9e9c73bef52
AgneseBettio <bettio.agnese@gmail.com>
2021-07-08 16:29:31 +0200

    inseriti dettagli utente

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -16,6 +16,16 @@
 
                     {{ __('Benvenuto utente registrato ' . Auth::user()->name . ' !') }}
                 </div>
+                <div class="card-body">
+                <ul>
+                    <li>nome: {{ Auth::user()->name}} </li>
+                    <li>email: {{ Auth::user()->email}} </li>
+                    <li>indirizzo: {{Auth::user()->detail->address}} - {{Auth::user()->detail->zip}} </li>
+                    <li>data di nascita: {{Auth::user()->detail->birthDate}} </li>
+                    <li>nazione: {{Auth::user()->detail->birthCountry}} </li>
+                </ul>
+
+                </div>
             </div>
         </div>
     </div>
